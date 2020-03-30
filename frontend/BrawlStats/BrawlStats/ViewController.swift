4ed4f6b30f757910d803a0257f5443219e7fd49f
Nuo (Tony) Chen <chen2886@purdue.edu>
2020-03-30 18:56:06 -0400

    Created XCode Project

---
+++ frontend/BrawlStats/BrawlStats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BrawlStats
+//
+//  Created by Tony Chen on 3/30/20.
+//  Copyright © 2020 BrawlStats-Purdue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
